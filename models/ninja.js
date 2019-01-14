286bb93023572dacdf6ffd1bba07336d99ad59cc
mick roman <mickroman65@gmail.com>
2019-01-14 23:44:27 +0000

    MogoDB Models & Schemas

---
+++ models/ninja.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema= mongoose.Schema;
+
+// create ninja Schema & model
+const NinjaSchema= newSchema({
+    name: {
+        type: String,
+        requires: [true, 'Name field is required']
+    },
+    rank: {
+        type: String
+    },
+    available:{
+        type: Boolean,
+        default: false
+    }
+    // add in geo location
+});
+
+const Ninja= mongoose.model('ninja', NinjaSchema);
+
+module.export= Ninja;
